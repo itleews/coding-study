8e1911e5347dfdf08fcb388f64c641ac87fc3171
Woosik Lee <leews.it@gmail.com>
2024-11-28 22:03:11 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    char str[n + 1];
+    scanf("%s", str);
+    int sum = 0;
+    for(int i = 0; i < n; i++)
+        sum += str[i] - '0';
+    printf("%d", sum);
+}
