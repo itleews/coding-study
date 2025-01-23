d0bdb2a83f1a9c2bedef0d53e95d40644f92a944
Woosik Lee <leews.it@gmail.com>
2025-01-23 14:35:44 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250123/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void func(int num) {
+    if (num % 2 == 0) {
+        if ((num / 10 + num % 10) % 5 == 0) {
+            printf("Yes");
+        }
+    }
+    else {
+        printf("No");
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    func(n);
+    return 0;
+}
