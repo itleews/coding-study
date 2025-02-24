c171cb72ab5dc48e75a1f913f5cb30af586813a8
Woosik Lee <leews.it@gmail.com>
2025-02-24 16:27:00 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -1,13 +1,9 @@
 #include <iostream>
 using namespace std;
 
-long long at(int a, int b) {
-    return ((a + b) * (a - b));
-}
-
 int main() {
     long long A, B;
     cin >> A >> B;
-    cout << at(A, B) << endl;
+    cout << ((A + B) * (A - B)) << endl;
     return 0;
 }
