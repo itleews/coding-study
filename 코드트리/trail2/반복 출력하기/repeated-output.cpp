f8beb2fe04d7cb2af544353106da8ff2cd78b609
Woosik Lee <leews.it@gmail.com>
2026-06-13 19:47:25 +0900

    [Passed] 반복 출력하기 (2ms, 0MB)

---
+++ 코드트리/trail2/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    cin >> n;
+
+    for (int idx = 0; idx < n; idx++)
+    {
+        cout << "12345^&*()_" << endl;
+    }
+    
+    return 0;
+}
