8b6ba58a1639b46ae32e32f467f7700eb8817530
Woosik Lee <leews.it@gmail.com>
2026-06-13 19:55:28 +0900

    [Passed] 숫자로 이루어진 사각형 (2ms, 0MB)

---
+++ 코드트리/trail2/숫자로 이루어진 사각형/rectangle-with-a-number.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void PrintRect(int n)
+{
+    int num = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << num << " ";           
+            num++;
+            if (num > 9)
+            {
+                num = 1;
+            }
+        }
+        cout << '\n';
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    PrintRect(n);
+    return 0;
+}
